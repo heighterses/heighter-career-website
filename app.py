9ce065fdc5eb0329a8a0f05aa84d1dd96344678b
heighterses <110668234+heighterses@users.noreply.github.com>
2023-07-25 11:04:48 +0000

    page changes

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
-from database import load_jobs_from_db
-
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  jobs= load_jobs_from_db()
-  return render_template("home.html", jobs=jobs,        company_name='heighterbyte')
+    jobs = load_jobs_from_db()
+    return render_template("home.html", jobs=jobs, company_name='heighterbyte')
 
+@app.route("/job/<id>")  # Convert 'id' to an integer
+def show_job(id):
+    job = load_job_from_db(id)
+    return render_template('jobpage.html', job=job)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -11,7 +11,20 @@ engine = create_engine(db_connection_string, connect_args={
 
 def load_jobs_from_db():
     with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
+        result = conn.execute(text("SELECT * FROM jobs"))
         column_names = result.keys()
         jobs = [dict(zip(column_names, row)) for row in result]
     return jobs
+  
+  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
